40ab547c28f07b626853b9a76e75912cd598e116
Keane <KeaneCarotenuto@gmail.com>
2020-11-23 13:03:11 +1300

    Camera Follows Player

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public GameObject toFollow;
+
+    void Start()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position, new Vector3(toFollow.transform.position.x, toFollow.transform.position.y, -10), 0.1f);
+    }
+}
